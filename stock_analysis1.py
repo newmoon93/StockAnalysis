4e89218335163d148f96830bcd4071478e0c0a7c
newmoon93 <seongwon93@gmail.com>
2023-02-13 23:31:41 +0900

    Real time stock data

---
+++ stock_analysis1.py
@@ -0,0 +1,22 @@
+#https://www.youtube.com/watch?v=q_MWWVgghsQ
+import pandas as pd
+import datetime
+import requests
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup
+
+def web_content_div(web_content, class_path):
+    web_content_div = web_content.find_all('div', {'calss': class_path})
+    try:
+        spans = web_content_div[0].find_all('span')
+        texts = [spans.get_text() for span in spans]
+    except IndexError:
+        texts = []
+    return texts
+
+def real_time_price(stock_code):
+    url =
+    try:
+        r = requests.get(url)
+        web_content = BeautifulSoup(r.text, 'lxml')
+        texts = web_content_div(web_content, 'class_path')
